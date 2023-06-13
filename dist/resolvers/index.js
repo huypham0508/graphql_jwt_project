204e7e160caa611c94ef31e71955e88ac7bbf6c0
huypham0508 <phammanhhuy1107@gmail.com>
2023-06-13 16:11:33 +0700

    build source

--- dist/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GreetingResolver = exports.UserResolver = void 0;
-const greeting_1 = require("./greeting");
-Object.defineProperty(exports, "GreetingResolver", { enumerable: true, get: function () { return greeting_1.GreetingResolver; } });
-const user_1 = require("./user");
-Object.defineProperty(exports, "UserResolver", { enumerable: true, get: function () { return user_1.UserResolver; } });
-//# sourceMappingURL=index.js.map
