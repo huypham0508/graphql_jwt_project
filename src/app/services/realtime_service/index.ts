89d25459156ba5aaf13f5fe98e6c975c8403c9ba
Mạnh Huy <phammanhhuy1107@gmail.com>
2025-01-13 11:26:38 +0700

    [remove][remove socket.io]

--- src/app/services/realtime_service/index.ts
+++ src/app/services/realtime_service/index.ts
@@ -1,46 +1,46 @@
-import { Server, Socket } from "socket.io";
+// import { Server, Socket } from "socket.io";
 
-export const getReceiverSocketId = (receiverId: string) => {
-  return connectedUsers.get(receiverId);
-};
+// export const getReceiverSocketId = (receiverId: string) => {
+//   return connectedUsers.get(receiverId);
+// };
 
-const connectedUsers: Map<string, Socket> = new Map();
+// const connectedUsers: Map<string, Socket> = new Map();
 
-const handleRealtimeConnect = async (httpServer: any) => {
-  const io = new Server(httpServer, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-  try {
-    io.on("connection", (socket: Socket) => {
-      socket.on("login", (userId: string) => {
-        connectedUsers.set(userId, socket);
-        console.log(`User ${userId} connected`);
-      });
+// const handleRealtimeConnect = async (httpServer: any) => {
+//   const io = new Server(httpServer, {
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//     },
+//   });
+//   try {
+//     io.on("connection", (socket: Socket) => {
+//       socket.on("login", (userId: string) => {
+//         connectedUsers.set(userId, socket);
+//         console.log(`User ${userId} connected`);
+//       });
 
-      socket.on("logout", (userId: string) => {
-        connectedUsers.delete(userId);
-        console.log(`User ${userId} disconnected`);
-      });
+//       socket.on("logout", (userId: string) => {
+//         connectedUsers.delete(userId);
+//         console.log(`User ${userId} disconnected`);
+//       });
 
-      socket.on("disconnect", () => {
-        console.log("A client disconnected");
-        connectedUsers.forEach((value, key) => {
-          if (value === socket) {
-            console.log({
-              value,
-              socket,
-            });
-            connectedUsers.delete(key);
-          }
-        });
-      });
-    });
-  } catch (error) {
-    console.log({ error });
-  }
-};
-export { connectedUsers };
-export default handleRealtimeConnect;
+//       socket.on("disconnect", () => {
+//         console.log("A client disconnected");
+//         connectedUsers.forEach((value, key) => {
+//           if (value === socket) {
+//             console.log({
+//               value,
+//               socket,
+//             });
+//             connectedUsers.delete(key);
+//           }
+//         });
+//       });
+//     });
+//   } catch (error) {
+//     console.log({ error });
+//   }
+// };
+// export { connectedUsers };
+// export default handleRealtimeConnect;
