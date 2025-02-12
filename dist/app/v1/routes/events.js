702825cfea8ceffa33567a64efa96c729e241297
Mạnh Huy <phammanhhuy1107@gmail.com>
2025-02-12 14:11:18 +0700

    [remove][remove dist]

--- dist/app/v1/routes/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const events_controller_1 = require("../controllers/events.controller");
-const events = (0, express_1.Router)();
-events.get("", events_controller_1.handleEvents);
-events.post("/register", events_controller_1.handleEventRegister);
-exports.default = events;
-//# sourceMappingURL=events.js.map
--- dist/app/v1/routes/refresh_token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const refresh_token_controller_1 = require("../controllers/refresh_token.controller");
-const refreshToken = (0, express_1.Router)();
-refreshToken.get("/", refresh_token_controller_1.handleRefreshToken);
-exports.default = refreshToken;
-//# sourceMappingURL=refresh_token.js.map
