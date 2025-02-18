08dd14c983eef1a143d8f7ab7333cdd18e9a6176
halo <halo@gmail.com>
2025-02-18 08:36:44 +0700

    sửa file

--- src/app/core/services/connect_database.service.ts
+++ src/app/core/services/connect_database.service.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { ConfigMongo } from "../../config";
-
-dotenv.config();
-const connectDBService: any = async () => {
-  try {
-    await mongoose.connect(ConfigMongo.URI_DATABASE, {
-      // useCreatendex: true,
-      // useFindAndModify: false,
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true
-    });
-    console.clear();
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
-};
-export default connectDBService;
-=======
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { ConfigMongo } from "../../config";
@@ -43,4 +20,3 @@ const connectDBService: any = async () => {
   }
 };
 export default connectDBService;
->>>>>>> f852b7e6f9d38f0c2e1e28c2bdf542c26378871b
